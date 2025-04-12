49d037914cc475b54dda18cf69fe9312d7eb7de4
Noe Liceaga <nliceagar@gmail.com>
2025-04-12 12:30:36 -0600

    feat: Initialize the project

---
+++ frontend/src/App.vue
@@ -0,0 +1,16 @@
+<!-- src/App.vue -->
+<template>
+  <v-app>
+    <v-main>
+      <RouterView />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
+
+<style>
+/* Global styles si quieres */
+</style>
