09ff9fd2e563f714ccc873fa3b7f7f22043eb066
Noe Liceaga <nliceagar@gmail.com>
2025-04-22 11:00:51 -0600

    feat: core app urls configuration

---
+++ backend/core/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import (
+    VideoViewSet,
+    ViewCreateView,
+    FavoriteCreateDestroyView,
+    CommentListCreateView,
+)
+
+router = DefaultRouter()
+router.register(r'videos', VideoViewSet, basename='video')
+
+url_patterns = [
+    path('', include(router.urls)),
+    path('view/', ViewCreateView.as_view(), name='view-create'),
+    path('favorite/', FavoriteCreateDestroyView.as_view(), name='favorite-create-destroy'),
+    path('comment/', CommentListCreateView.as_view(), name='comment-list-create'),
+]   
