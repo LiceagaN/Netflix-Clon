57c233c980a0cd06b3e2dc7ced7bda4dc1f07d6a
Noe Liceaga <nliceagar@gmail.com>
2025-04-22 10:56:37 -0600

    feat: Views created

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets, generics, filters
+from .models import User, Video, Tag, View, Favorite, Comment, Category
+from .serializers import (
+    VideoSerializer,
+    TagSerializer,
+    ViewSerializer,
+    FavoriteSerializer,
+    CommentSerializer,
+    CategorySerializer
+)
 # Create your views here.
+class VideoViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Video.objects.all()
+    serializer_class = VideoSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'description']
+
+class ViewCreateView(generics.CreateAPIView):
+    serializer_class = ViewSerializer
+
+class FavoriteCreateDestroyView(generics.CreateAPIView, generics.DestroyAPIView):
+    serializer_class = FavoriteSerializer
+    queryset = Favorite.objects.all()
+
+class CommentListCreateView(generics.ListCreateAPIView):
+    serializer_class = CommentSerializer
+
+    def get_queryset(self):
+        video_id = self.request.query_params.get('video_id')
+        return Comment.objects.filter(video_id=video_id) if video_id else Comment.objects.all()
